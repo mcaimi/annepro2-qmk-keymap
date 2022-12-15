384ec8a187a86b01bbfe8202cb62202c37e06de5
Marco Caimi <mcaimi@redhat.com>
2022-12-15 16:56:35 +0100

    Update QMK+Shine build script

--- qmk_build/build.sh
+++ qmk_build/build.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+NAME=$1
+KEYMAP_NAME=${NAME:-custom_layout}
 C15_BOARD=/qmk_firmware/keyboards/annepro2/c15
 C15_KEYMAP=/qmk_firmware/keyboards/annepro2/keymaps
 
 # setup qmk firmware
 qmk setup -y || exit -1
-git clone https://github.com/OpenAnnePro/AnnePro2-Shine.git --recursive --depth 1
+qmk setup -H /qmk_firmware/
+qmk config user.keyboard=annepro2/c15
+qmk config user.keymap=$KEYMAP_NAME
+qmk new-keymap
 
 # copy custom configuration from volume
 cp -v /custom/boards/C15/config.h $C15_BOARD/config.h
 cp -v /custom/boards/C15/rules.mk $C15_BOARD/rules.mk
+cp -v /custom/boards/C15/info.json $C15_BOARD/info.json
 
 # copy custom keymap
-cp -vr /custom/keymap $C15_KEYMAP/
+cp -vr /custom/keymap $C15_KEYMAP/$KEYMAP_NAME/
 
-# build firmware
-cd /qmk_firmware
-make annepro2/c15:keymap
+# build keyboard firmware
+qmk compile
+
+# build shine (rgb ligting) firmware
+git clone https://github.com/OpenAnnePro/AnnePro2-Shine.git --recursive --depth 1 || exit 1
 cd /AnnePro2-Shine
 make C15
 
 # copy built firmware
-cp -v /qmk_firmware/annepro2_c15_keymap.bin /target/annepro2_c15_keymap_$(date +%F).bin
+cp -v /qmk_firmware/annepro2_c15_$KEYMAP_NAME.bin /target/annepro2_c15_$KEYMAP_NAME_$(date +%F).bin
 cp -v /AnnePro2-Shine/build/annepro2-shine-C15.bin /target/annepro2_c15_shine_$(date +%F).bin
